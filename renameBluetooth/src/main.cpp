f1efff5091e7e0b0b6986ebf5d089b63fea90226
Alexander Zozulia <aleksandr.zozulya.1997@gmail.com>
2025-05-13 23:57:33 +0300

    Slightly formatted version for renaming bluetooth module

--- renameBluetooth/src/main.cpp
+++ renameBluetooth/src/main.cpp
@@ -1,28 +1,33 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
-SoftwareSerial bt(2, 3); // RX, TX
+SoftwareSerial bt(2, 3); // RX (to HC-05 TX), TX (to HC-05 RX)
 
 String inputName = "";
 bool nameSent = false;
 
-void chooseBtName(); // Вызывать функцию в loop
-void changeBtName(); // Вызывать функцию в setup
+void chooseBtName(); // Call this in loop()
+void changeBtName(); // Call this in setup() if needed
 
 void setup()
 {
-  bt.begin(9600);       // Скорость для AT-команд (обычно 9600)
-  Serial.begin(115200); // Для отладки
+  bt.begin(9600);       // Baud rate for AT commands (usually 9600)
+  Serial.begin(115200); // For serial monitor communication
 
   delay(1000);
+
+  // Uncomment the line below to send a fixed name automatically at startup:
+  // changeBtName();
 }
 
 void loop()
 {
+  chooseBtName();
 }
 
 void chooseBtName()
 {
+  // Read input from serial monitor
   if (Serial.available())
   {
     char c = Serial.read();
@@ -33,9 +38,9 @@ void chooseBtName()
         String command = "AT+NAME=" + inputName;
         bt.println(command);
 
-        Serial.print("Отправлена команда: ");
+        Serial.print("Command sent: ");
         Serial.println(command);
-        nameSent = true; // Больше не отправлять повторно
+        nameSent = true; // Prevent resending
       }
     }
     else
@@ -44,23 +49,24 @@ void chooseBtName()
     }
   }
 
-  // Ответ от bt-модуля
+  // Read response from the BT module
   if (bt.available())
   {
     char c = bt.read();
     Serial.print(c);
   }
 }
+
 void changeBtName()
 {
   Serial.println("Sending rename command...");
 
-  // Переименовать модуль на "My_BT_Device"
+  // Rename the module to "My_BT_Device"
   bt.println("AT+NAME=My_BT_Device");
 
-  delay(1000); // Ждем ответ
+  delay(1000); // Wait for a response
 
-  // Показать ответ от модуля
+  // Print response from the BT module
   while (bt.available())
   {
     char c = bt.read();
