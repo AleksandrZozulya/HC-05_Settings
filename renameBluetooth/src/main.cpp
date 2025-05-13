3eb3ad3993881176bb4d951eff95e3d9967abcb4
Alexander Zozulia <aleksandr.zozulya.1997@gmail.com>
2025-05-13 23:54:56 +0300

    modify code include 2 options

--- renameBluetooth/src/main.cpp
+++ renameBluetooth/src/main.cpp
@@ -1,30 +1,71 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
-SoftwareSerial bluetooth(2, 3); // RX, TX
+SoftwareSerial bt(2, 3); // RX, TX
 
-void setup() {
-  bluetooth.begin(9600);      // Скорость для AT-команд (обычно 9600)
-  Serial.begin(115200);       // Для отладки
+String inputName = "";
+bool nameSent = false;
 
-  delay(1000); // Небольшая задержка перед началом
+void chooseBtName(); // Вызывать функцию в loop
+void changeBtName(); // Вызывать функцию в setup
 
+void setup()
+{
+  bt.begin(9600);       // Скорость для AT-команд (обычно 9600)
+  Serial.begin(115200); // Для отладки
+
+  delay(1000);
+}
+
+void loop()
+{
+}
+
+void chooseBtName()
+{
+  if (Serial.available())
+  {
+    char c = Serial.read();
+    if (c == '\n' || c == '\r')
+    {
+      if (inputName.length() > 0 && !nameSent)
+      {
+        String command = "AT+NAME=" + inputName;
+        bt.println(command);
+
+        Serial.print("Отправлена команда: ");
+        Serial.println(command);
+        nameSent = true; // Больше не отправлять повторно
+      }
+    }
+    else
+    {
+      inputName += c;
+    }
+  }
+
+  // Ответ от bt-модуля
+  if (bt.available())
+  {
+    char c = bt.read();
+    Serial.print(c);
+  }
+}
+void changeBtName()
+{
   Serial.println("Sending rename command...");
 
   // Переименовать модуль на "My_BT_Device"
-  bluetooth.println("AT+NAME=My_BT_Device");
+  bt.println("AT+NAME=My_BT_Device");
 
   delay(1000); // Ждем ответ
 
   // Показать ответ от модуля
-  while (bluetooth.available()) {
-    char c = bluetooth.read();
+  while (bt.available())
+  {
+    char c = bt.read();
     Serial.print(c);
   }
 
   Serial.println("\nRename command sent.");
-}
-
-void loop() {
-  // Ничего не делаем
 }
